9c228053e47fb0b598c417ee56340493f7c6c288
Zigashin2014 <ziganshin.yusuf@mail.ru>
2026-08-02 21:40:09 +0300

    (Homework№7) worked JS

---
+++ homework-7.js
@@ -0,0 +1,37 @@
+function StateTheTemperature (c, t) {
+    console.log(`Сейчас в ${c} температура — ${t} градусов по Цельсию`)
+}
+
+StateTheTemperature ('Москва', '20')
+
+function CheckSpeed (speed) {
+    if (speed == 299792458) {
+        console.log('Световая скорость')
+    }
+    if (speed > 299792458) {
+        console.log('Сверхсветовая скорость')
+    }
+    if (speed < 299792458) {
+        console.log('Субсветовая скорость')
+    }
+}
+
+CheckSpeed ('299792458')
+const product = "milk"
+const price = 89
+
+function BuyProduct (Budget) {
+    if (Budget >= price) {
+        console.log('${product} приобретён. Спасибо за покупку!')    
+    } else  { console.log('Вам не хватает 1рубль,  пополните баланс') }
+}
+
+BuyProduct ('9')
+
+function test () {
+
+}
+
+const test1 = 1
+let test2 = 1
+var test3 = 1
