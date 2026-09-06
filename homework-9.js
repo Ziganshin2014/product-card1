28146b969db2496b25de14c07d0e4bca3c9f8af6
Zigashin2014 <ziganshin.yusuf@mail.ru>
2026-09-06 11:21:25 +0300

    Homework - 9

---
+++ homework-9.js
@@ -0,0 +1,34 @@
+const arrayNumbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+const newArray = arrayNumbers.filter(Number => Number> 4 )
+
+console.log(newArray)
+
+const movies = [
+  "Побег из Шоушенка",
+  "Крёстный отец",
+  "Тёмный рыцарь",
+  "Криминальное чтиво",
+  "Властелин колец: Братство Кольца",
+  "Форрест Гамп",
+  "Начало",
+  "Матрица",
+  "Бойцовский клуб",
+  "Интерстеллар"
+];
+
+const newMovies = movies.filter(movie => movie.includes("Интерстеллар"))
+
+console.log(newMovies)
+
+movies.reverse()
+
+console.log(movies)
+
+arrayNumbers.reverse()
+
+console.log(arrayNumbers)
+
+
