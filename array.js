1dbfe0ee60a7d71163056880a5380ec66b6c1178
Zigashin2014 <ziganshin.yusuf@mail.ru>
2026-09-19 10:53:41 +0300

    Homework

---
+++ array.js
@@ -0,0 +1,9 @@
+export const products = [
+  { title: 'Кофе', price: 1290 },
+  { title: 'Чай', price: 540 },
+  { title: 'Кружка', price: 890 },
+  { title: 'Термокружка', price: 1490 },
+  { title: 'Сироп', price: 350 }
+];
+
+const reduceProducts = comments.reduce((acc,pro) => {acc.push(pro); return acc   },[] )
